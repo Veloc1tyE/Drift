6e43b96627c5d5ecbb1939d3e2fe4f51f4c88708
Will Jeremijenko <45901181+Veloc1tyE@users.noreply.github.com>
2019-12-31 17:21:14 +1000

    Update storage.h

--- include/drift/storage.h
+++ include/drift/storage.h
@@ -9,14 +9,24 @@
 #include <drift/spaces.h>
 
 namespace drift {
+    
+    /**
+     * When the agent is initialised, we want it to be able to store all the information
+     * it needs for training effectively, this class provides the infrastructure to do so
+     */
+    
     class RolloutStorage
     {
+        // the A2C algorithm relies on tensors containing the following information
         private:
+            // these represent the agent's knowledge of its environment
             torch::Tensor observations, hidden_states, rewards,
                 value_predictions, action_log_probs, masks;
-            torch::Device device;
-            int64_t num_steps;
+            // training related information
+            torch::Device device; 
+            int64_t num_steps;    
             int64_t step;
+        
         public:
             RolloutStorage(int64_t num_steps,
                         int64_t num_processes,
