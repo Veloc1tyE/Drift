07d2d24f79d1cb63a6602b64eb8c7620d91c97b4
Veloc1ty <veloc1ty@localhost.localdomain>
2019-12-31 17:13:52 +1000

    ensuring successful build

---
+++ include/drift/spaces.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <torch/torch.h>
+
+namespace drift  {
+
+    /**
+     * Define an action space which contains a type of action
+     * the agent can perform, as well as a vector containing integers
+     * which encodes information for the action.
+     * 
+     * TODO: explain why it's useful
+     *
+     */ 
+    
+    struct ActionSpace  {
+        std::string type;
+        std::vector<int64_t> shape;
+    }
+}
