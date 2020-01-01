983919400ca8be494070a15788367a01f28cf2f7
Veloc1ty <veloc1ty@localhost.localdomain>
2020-01-02 00:28:14 +1000

    A2C implementation abstractions

---
+++ include/drift/model/output_layers.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <memory>
+#include <torch/torch.h>
+#include "drift/distributions/distribution.h"
+
+using namespace torch;
+
+namespace drift {
+    
+    /*
+     * Actor/Critic networks map to an output layer over an action space
+     * These will contain some probability distribution for the given space
+     */ 
+
+    class OutputLayer : public nn:Module {
+        public:
+            virtual ~OutputLayer() = 0;
+            // Forward is just a reference to the output distribution
+            virtual std::unique_ptr<Distribution> forward(torch::Tensor x) = 0;
+    };
+
+    inline OutputLayer::OutputLayer() {};
+
+    /*
+     * Output may map to a Bernoulli distribution
+     */ 
+
+    class BernoulliOutput : public OutputLayer {
+        private:
+            nn::Linear linear;
+
+        public:
+            // We construct a Bernoulli mapping over the number of outputs given the input shape
+            BernoulliOutput(unsigned int num_inputs, unsigned int num_outputs);
+            
+            // Reference output distribution flowing from the inputs
+            std::unique_ptr<Distribution> forward(torch::Tensor x);
+    };
+    
+    /*
+     * Output may map to a Categorical distribution
+     */ 
+
+    class CategoricalOutput : public OutputLayer {
+        private:
+            nn::Linear linear;
+
+        public:
+            CategoricalOutput(unsigned int num_inputs, unsigned int num_outputs);
+            std::unique_ptr<Distribution> forward(torch::Tensor x);
+    };
+
+    /*
+     * We may have a normally distributed output distribution
+     */ 
+
+    class NormalOutput : public OutputLayer {
+        private:
+            // Normal distribution is defined by its mean and variance
+            nn::Linear linear_loc;
+            torch::Tensor scale_log;
+
+        public:
+            NormalOutput(unsigned int num_inputs, unsigned int num_outputs);
+
+            std::unique_ptr<Distribution> forward(torch::Tensor x);
+
+    };
+
+    /*
+     * Note: Further distributions are possible, like a normalising flow, to be implemented later
+     */ 
+
+}
+
+
---
+++ include/drift/model/policy.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <torch/torch.h>
+
+#include "drift/model/nn_base.h"
+#include "drift/model/output_layer.h"
+#include "drift/observation_normalizer.h"
+#include "drift/spaces.h"
+
+using namespace torch;
+
+/*
+ * Construct for Model Policy
+ */ 
+
+namespace drift {
+
+    class PolicyImpl : public nn::Module {
+        private:
+            /* 
+             * Policy requires an action_space, base for its neural-network model,
+             * an observation_normalizer for observations flowing through this base,
+             * and an output layer which defines the probability distribution over the action_space
+             */
+
+            // store the action space
+            ActionSpace action_space;
+            // reference to nn-base
+            std::shared_ptr<NNBase> base;
+            // normalise observations
+            ObservationNormalizer observation_normalizer;
+            // reference to nn-output-layer
+            std::shared_ptr<OutputLayer> output_layer;
+            
+            // forward operation, assuming recurrency (take into account past information)
+            std::vector<torch::Tensor> forward_gru(torch::Tensor x,
+                                                   torch::Tensor y,
+                                                   torch::Tensor masks);
+        public:
+            // Implement the policy constructor
+            PolicyImpl(ActionSpace action_space,
+                       std::shared_ptr<NNBase> base,
+                       bool normalize_observations = false);
+            
+            // Take an action given the inputs, rnn hidden states and masks
+            std::vector<torch::Tensor> act(torch::Tensor inputs,
+                                           torch::Tensor rnn_hxs,
+                                           torch::Tensor masks) const;
+            
+            // Evaluate the action taken based on the network inputs according to associated returns
+            std::vector<torch::Tensor> evaluate_actions(torch::Tensor inputs,
+                                                        torch::Tensor rnn_hxs,
+                                                        torch::Tensor masks,
+                                                        torch::Tensor actions) const;
+
+            // retrieve the probability distribution over the action-space given network inputs
+            torch::Tensor get_probs(torch::Tensor inputs,
+                                    torch::Tensor rnn_hxs,
+                                    torch::Tensor masks) const;
+
+            // retrieve values produced from sampling probability distributions
+            torch::Tensor get_values(torch::Tensor inputs,
+                                     torch::Tensor rnn_hxs,
+                                     torch::Tensor masks) const;
+
+            // mean and variance of normaliser changes following each observation
+            void update_observation_normalizer(torch::Tensor observation);
+            
+            inline bool is_recurrent() const { return base->is_recurrent(); }
+            
+            inline unsigned int get_hidden_size() const {
+                return base->get_hidden_size();
+            }
+            
+            inline bool using_observation_normalizer() const { 
+                return base->using_observation_normalizer();
+            }
+    };
+
+    // register policy to torch-module to enable easy tracking
+    TORCH_MODULE(policy);
+
+}
