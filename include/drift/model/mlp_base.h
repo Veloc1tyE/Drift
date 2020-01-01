fd6728ecbd152b4f9595b44d4c0771b64d807208
Veloc1ty <veloc1ty@localhost.localdomain>
2020-01-01 17:13:38 +1000

    Implement abstractions for constructing neural networks

---
+++ include/drift/model/mlp_base.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <torch/torch.h>
+#include "drift/model/nn.base.h"
+
+using namespace torch;
+
+namespace drift {
+    
+    
+    class MlpBase : public NNBase {
+        
+        /*
+         * An MLP for use in the A2C algorithm contains an actor, critic,
+         * and requires a list of inputs
+         */
+
+        private:
+            nn::Sequential actor;
+            nn::Sequential critic;
+            nn::Linear critic_linear;
+            unsigned int num_inputs;
+
+        public:
+            
+            MlpBase(unsigned int num_inputs,
+                    bool recurrent = false;
+                    unsigned int hidden_size = 64);
+
+            std::vector<torch::Tensor> forward(torch::Tensor inputs,
+                                               torch::Tensor hxs,
+                                               torch::Tensor masks);
+            
+            inline unsigned int get_num_inputs() const { return num_inputs; }
+    };
+}
---
+++ include/drift/model/nn_base.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vector>
+#include <torch/torch.h>
+
+using namespace torch;
+
+namespace drift  {
+    
+    /*
+     * Abstract class used to contruct Neural Networks
+     */ 
+
+    class NNBase : public nn::Module {
+        private:
+            // generalise recurrent unit, required for models based on recurrency
+            nn::GRU gru;
+
+            // attributes of NN
+            unsigned int hidden_size;
+            bool recurrent;
+
+        public:
+            /* required methods
+             */
+
+            // constructor
+            NNBase(bool recurrent,
+                   unsigned int recurrent_input_size,
+                   unsigned int hidden_size);
+            
+            // mapping input to output, here virtual is used as this is generally applicable
+            virtual std::vector<torch::Tensor> forward(torch::Tensor inputs,
+                                                       torch::Tensor hxs,
+                                                       torch::Tensor masks);
+            // similar mapping but with recurrency
+            std::vector<torch::Tensor> forward_gru(torch::Tensor inputs,
+                                                   torch::Tensor hxs,
+                                                   torch::Tensor masks);
+            // retrieve info about NN
+            unsigned int get_hidden_sizes() const;
+            inline int get_output_size() const { return hidden_size; }
+            inline bool is_recurrent() const { return recurrent; }
+    };
+
+}
